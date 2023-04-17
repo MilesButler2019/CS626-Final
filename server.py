3415a44bc588f697605cb897c5a9192850e5f868
Miles Butler <47306315+MilesButler2019@users.noreply.github.com>
2023-04-17 19:52:09 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import torch
 from model import Net
+import json
 
 app = flask.Flask(__name__)
 
@@ -8,22 +9,13 @@ app = flask.Flask(__name__)
 
 global_model = torch.load('model_checkpoint.pt')
 
-@app.route('/update_model', methods=['POST'])
-def update_model():
-     
-    torch.save(global_model, 'global_model.pt')
-      
-#     client_model_params = flask.request.get_json()
-#     client_model = Net()
-#     client_model.load_state_dict(client_model_params)
-#     global_model_params = global_model.state_dict()
-#     for key in global_model_params:
-#         global_model_params[key] += client_model.state_dict()[key]
-#     global_model.load_state_dict(global_model_params)
+@app.route('/get_global_model', methods=['GET'])
+def get_global_model():
+    # Serialize the global model to a JSON string
+    global_model_json = json.dumps(global_model.state_dict())
 
-
-
-    return 'Model updated'
+    # Return the serialized global model as a response to the client
+    return global_model_json
 
 
 
