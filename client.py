58e619ea899ac85e12765cab02dbf8698f52693f
Miles Butler <47306315+MilesButler2019@users.noreply.github.com>
2023-04-17 19:57:05 -0400

    Update client.py

--- client.py
+++ client.py
@@ -8,16 +8,15 @@ local_data = ...
 
 
 
-
-local_model = Net()
+model = request.get('http://3.84.112.131/get_global_model')
 
 for epoch in range(num_epochs):
-    local_model_params = local_model.state_dict()
+    local_model_params = model.state_dict()
     optimizer = optim.SGD(local_model.parameters(), lr=lr)
     optimizer.zero_grad()
     output = local_model(local_data)
     loss = F.nll_loss(output, target)
     loss.backward()
     optimizer.step()
-    requests.post('http://server_ip:5000/update_model', json=local_model_params)
+#     requests.post('http://server_ip:5000/update_model', json=local_model_params)
     tqdm.write(f'Epoch {epoch+1}/{num_epochs}: Loss={loss.item():.4f}')
