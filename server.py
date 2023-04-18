a75ed74c996ba1b239a93154f6c1088654460e28
Miles Butler <47306315+MilesButler2019@users.noreply.github.com>
2023-04-17 20:18:55 -0400

    Update server.py

--- server.py
+++ server.py
@@ -12,11 +12,17 @@ global_model = torch.load('model_checkpoint.pt')
 
 @app.route('/get_global_model', methods=['GET'])
 def get_global_model():
-    # Serialize the global model to a JSON string
-    global_model_json = json.dumps(global_model)
+    # Extract the model parameters and convert them to a dictionary
+    model_params = global_model.state_dict()
+    model_params_dict = {}
+    for param_name, param_tensor in model_params.items():
+        model_params_dict[param_name] = param_tensor.tolist()
 
-    # Return the serialized global model as a response to the client
-    return global_model_json
+    # Serialize the model parameters dictionary to a JSON string
+    model_params_json = json.dumps(model_params_dict)
+
+    # Return the serialized model parameters as a response to the client
+    return model_params_json
 
 
 
